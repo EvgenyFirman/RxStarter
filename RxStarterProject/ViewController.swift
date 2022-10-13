3a3aebc18776ffdd10d063ef05c5d752d2146238
Evgeny Firman <Eugeniolio@Yandex.ru>
2022-10-13 17:01:53 +0300

    Initial Commit

---
+++ RxStarterProject/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RxStarterProject
+//
+//  Created by Евгений Фирман on 13.10.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
